eb6a0329091c6d78264d2aa171e57a3e7c67f50a
Evgeniy Malygin <black2001@inbox.ru>
2023-04-02 22:10:59 +0300

    добавлена обработка ошибок

--- src/main/java/com/example/testbaseofsocks/controller/ApiSocksController.java
+++ src/main/java/com/example/testbaseofsocks/controller/ApiSocksController.java
@@ -4,9 +4,13 @@ import com.example.testbaseofsocks.dto.SocksDto;
 import com.example.testbaseofsocks.service.ApiSocksService;
 
 
+import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.*;
+
+
 
 @RestController
 @RequestMapping("/api/socks")
@@ -20,15 +24,17 @@ public class ApiSocksController {
     }
 
     @PostMapping("/income")
-    ResponseEntity<SocksDto> addSocks(@RequestBody() SocksDto properties)
+    ResponseEntity<Object> addSocks(@RequestBody() SocksDto properties)
     {
-        return ResponseEntity.status(HttpStatus.OK).body(apiSoksService.addSocksToDb(properties));
+        apiSoksService.addSocksToDb(properties);
+        return ResponseEntity.status(HttpStatus.OK).body("удалось добавить приход");
     }
 
     @PostMapping("/outcome")
-    ResponseEntity<SocksDto> decreaseSocks(@RequestBody SocksDto properties)
+    ResponseEntity<Object> decreaseSocks(@RequestBody SocksDto properties)
     {
-        return ResponseEntity.status(HttpStatus.OK).body(apiSoksService.decreaseSocks(properties));
+        apiSoksService.decreaseSocks(properties);
+        return ResponseEntity.status(HttpStatus.OK).body("удалось отгрузить носки");
     }
 
     @GetMapping()
@@ -36,9 +42,24 @@ public class ApiSocksController {
                                                              @RequestParam(name = "operation") String operation,
                                                              @RequestParam(name = "cottonPart") Double cottonPart)
     {
+        if(!color.matches("[a-zA-Z]+") || (!operation.equals("lessThan") && !operation.equals("moreThan") &&
+            !operation.equals("equal"))){
+        throw  new HttpMessageNotReadableException("параметры запроса отсутствуют или имеют некорректный формат");
+    }
+
         return ResponseEntity.status(HttpStatus.OK).body(apiSoksService.quantitySocksFromWarehouse(color,
                 operation, cottonPart));
     }
 
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorMessage> handleException() {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorMessage("параметры запроса отсутствуют " +
+                "или имеют некорректный формат"));
+    }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorMessage> handleException500() {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorMessage("произошла ошибка, " +
+                "не зависящая от вызывающей стороны (например, база данных недоступна)"));
+    }
 }
